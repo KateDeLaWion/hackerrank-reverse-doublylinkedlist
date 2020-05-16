547b7a5a5b95416fa14b2b913d48fd9020ae71a1
musicstudio <musicstudio@musics-MBP.home>
2020-05-15 23:20:34 -0400

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+    // https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
+
+//    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+
+
+//  // my second code starts here
+        if (head == null){
+          return head;
+        }
+        DoublyLinkedListNode current = head; // make a current node to traverse
+        DoublyLinkedListNode newHead = head;
+        while (current != null){
+        DoublyLinkedListNode previous = current.prev;
+        current.prev = current.next;
+        current.next = previous;
+        newHead = current;
+        current = current.prev; // travarse from right to left.
+        }
+        return newHead;
+//        // my code ends here
+
+
+
+//    }
+
+
+}
